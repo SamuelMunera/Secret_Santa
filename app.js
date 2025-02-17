17dec7799579a3636646c88ceae0f52f6466b4bd
SamuelMunera <samuelmunera11@gmail.com>
2025-02-16 19:19:56 -0500

    add readme and update the logic

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
-let amigos = []
+let amigos = [];
 
-function  agregarAmigos (){
-
-    const input =  document.getElementById( "amigo" )
-    const nombre = input.value.trim()
-    const listaAmigos = document.getElementById ("listaAmigos")
-
-    if (nombre === ""){
-         alert ("por favor ingresa un nombre")
-         return
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    const listaAmigos = document.getElementById("listaAmigos");
+    
+    
+  if (nombre === "") {
+        alert("Por favor, ingresa un nombre.");
+        return;
     }
     if (amigos.includes(nombre)) {
         alert("Este nombre ya está en la lista.");
         return;
     }
-    amigo.push(nombre);
-    const li = document.createElement ("li")
-    li.textContent = nombre; 
-    listaAmigos.appendChild(li)
-     
-    input.value = ""; 
+    
+    amigos.push(nombre);
+    const li = document.createElement("li");
+    li.textContent = nombre;
+    listaAmigos.appendChild(li);
+    
+    input.value = "";
 }
- 
-function sortearAmigo(){
-    if (amigos.length < 2){
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
         alert("Deben haber al menos dos participantes para sortear.");
         return;
     }
-
+    
     let asignaciones = new Map();
     let participantes = [...amigos];
     let sorteados = [...amigos];
-
-    for ( let participante of participantes ){
-        let posible = sorteados.filter(p => p !== participante);
-        if (posibles.length === 0 ){
-            return sortearAmigo();
+    
+    for (let participante of participantes) {
+        let posibles = sorteados.filter(p => p !== participante);
+        
+        if (posibles.length === 0) {
+            return sortearAmigo(); // Reintentar si se llega a un punto sin opciones
         }
-     
-        let seleccionado = posibles [Math.floor(Math.random() * posibles.length)]
-
+        
+        let seleccionado = posibles[Math.floor(Math.random() * posibles.length)];
+        asignaciones.set(participante, seleccionado);
+        
         sorteados = sorteados.filter(p => p !== seleccionado);
-
     }
-mostrarResultados(asignaciones)
+    
+    mostrarResultado(asignaciones);
 }
 
 function mostrarResultado(asignaciones) {
